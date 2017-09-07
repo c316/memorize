dd744f9f198f4a9fde9a00f3aab428aeda88d497
JoshJoe <josh@joshjoe.com>
2017-09-06 21:49:38 -0500

    Moved getWeek function into a miscFunctions file

--- Verses.js
+++ Verses.js
@@ -10,6 +10,8 @@ import {
 import { Icon } from 'react-native-elements';
 import { verses } from './assets/verses';
 
+import { getWeek } from './miscFunctions';
+
 class Verses extends React.Component {
 
   constructor(props) {
@@ -17,20 +19,7 @@ class Verses extends React.Component {
     this.state = {showVerse: 0};
   }
   componentDidMount() {
-
-    _getWeek = function(){
-      var date = new Date();
-      date.setHours(0, 0, 0, 0);
-      // Thursday in current week decides the year.
-      date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-      // January 4 is always in week 1.
-      var week1 = new Date(date.getFullYear(), 0, 4);
-      // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-      return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
-        - 3 + (week1.getDay() + 6) % 7) / 7);
-    }
-
-    let week = _getWeek();
+    let week = getWeek();
     let year = new Date().getFullYear();
     // Find the first verse that matches today's year and week
     const thisVerse = verses.find((verse) => {
