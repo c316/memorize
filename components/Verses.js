13e96f97f9ed0e20ecbc0c06ea39b8e8b1a8f223
Joshua Bechard <josh.bechard@gmail.com>
2018-09-24 15:57:31 -0500

    Make the verses scrollable, linting

--- components/Verses.js
+++ components/Verses.js
@@ -1,128 +1,129 @@
-import React from "react";
+import React from 'react';
 import {
   Dimensions,
   Text,
   View,
+  ScrollView,
   TouchableHighlight,
   Image,
   Platform,
   StyleSheet,
   Linking,
   SafeAreaView,
-  ImageBackground,
-} from "react-native";
-import { Icon } from "react-native-elements";
-import verses from "../assets/verses";
+  ImageBackground
+} from 'react-native';
+import { Icon } from 'react-native-elements';
+import verses from '../assets/verses';
 
-import { getWeek, moderateScale } from "../miscFunctions";
+import { getWeek, moderateScale } from '../miscFunctions';
 
-const { width } = Dimensions.get("window");
+const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   box: {
-    alignItems: "stretch",
-    width: "100%",
-    backgroundColor: "#7540EE",
+    alignItems: 'stretch',
+    width: '100%',
+    backgroundColor: '#7540EE',
     shadowOffset: { height: -1 },
-    shadowColor: "grey",
+    shadowColor: 'grey',
     shadowOpacity: 1,
-    elevation: -1,
+    elevation: -1
   },
   headerBox: {
     flex: 1,
-    width: "100%",
+    width: '100%'
   },
   container: {
-    display: "flex",
-    alignItems: "baseline",
-    height: "100%",
+    display: 'flex',
+    alignItems: 'baseline',
+    height: '100%'
   },
   imageStyle: {
     shadowOffset: { height: 6 },
-    shadowColor: "grey",
+    shadowColor: 'grey',
     shadowOpacity: 0.5,
-    overflow: "visible",
+    overflow: 'visible',
     height: 160,
-    alignSelf: "center",
-    marginTop: 50,
+    alignSelf: 'center',
+    marginTop: 50
   },
   instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5,
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
   },
   verseText: {
     flex: 1,
     fontSize: moderateScale(20),
-    alignSelf: "flex-start",
+    alignSelf: 'flex-start',
     marginLeft: width * 0.05,
     marginRight: width * 0.05,
     marginTop: 15,
     marginBottom: 35,
-    color: "rgba(37, 38, 94, 0.8)",
+    color: 'rgba(37, 38, 94, 0.8)'
   },
   verse: {
     fontSize: moderateScale(26),
-    color: "rgba(37, 38, 94, 1)",
+    color: 'rgba(37, 38, 94, 1)',
     letterSpacing: 0.78,
-    alignSelf: "center",
+    alignSelf: 'center'
   },
   verseDates: {
     fontSize: 24,
-    textAlign: "center",
-    color: "white",
+    textAlign: 'center',
+    color: 'white'
   },
   esvCredit: {
     margin: 8,
     fontSize: 9,
-    textAlign: "center",
-    color: "white",
+    textAlign: 'center',
+    color: 'white'
   },
   headerTitle: {
     flex: 2,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 10
   },
   headerUnderline: {
-    alignItems: "center",
-    borderBottomColor: "rgba(117, 64, 238, 1)",
+    alignItems: 'center',
+    borderBottomColor: 'rgba(117, 64, 238, 1)',
     borderBottomWidth: 2,
     shadowOffset: { height: 2 },
-    shadowColor: "rgba(117, 64, 238, 0.8)",
+    shadowColor: 'rgba(117, 64, 238, 0.8)',
     shadowOpacity: 1,
     elevation: 3,
-    backgroundColor: "black",
+    backgroundColor: 'black',
     borderRadius: 5,
     marginTop: 10,
-    marginBottom: 20,
+    marginBottom: 20
   },
   controlsBlock: {
     height: 125,
-    justifyContent: "flex-end",
-    alignItems: "stretch",
-    width: "100%",
-    backgroundColor: "#7540EE",
+    justifyContent: 'flex-end',
+    alignItems: 'stretch',
+    width: '100%',
+    backgroundColor: '#7540EE',
     shadowOffset: { height: -1 },
-    shadowColor: "grey",
+    shadowColor: 'grey',
     shadowOpacity: 1,
-    elevation: -1,
+    elevation: -1
   },
   verseBlock: {
     marginTop: 20,
-    alignSelf: "center",
+    alignSelf: 'center'
   },
   dot: {
     height: 14,
     width: 14,
     borderRadius: 14 / 2,
-    backgroundColor: "#FF7052",
+    backgroundColor: '#FF7052',
     left: 10,
     bottom: 15,
     borderWidth: 2,
-    borderColor: "white",
-  },
+    borderColor: 'white'
+  }
 });
 
 class Verses extends React.Component {
@@ -182,18 +183,18 @@ class Verses extends React.Component {
     const verseIndex = this.state.showVerse;
     const useThisVerse = verses[verseIndex];
     const monthNames = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
     ];
 
     const startDate = new Date(
@@ -224,89 +225,89 @@ class Verses extends React.Component {
 
   render() {
     const useThisVerse = verses[this.state.showVerse];
-    const endVerse = useThisVerse.endVerse ? `-${useThisVerse.endVerse}` : "";
+    const endVerse = useThisVerse.endVerse ? `-${useThisVerse.endVerse}` : '';
     let bookImagePath;
     switch (useThisVerse.book) {
-      case "Exodus":
-        bookImagePath = require("../assets/images/bible/Exodus.png");
+      case 'Exodus':
+        bookImagePath = require('../assets/images/bible/Exodus.png');
         break;
-      case "1 Corinthians":
-        bookImagePath = require("../assets/images/bible/1Corinthians.png");
+      case '1 Corinthians':
+        bookImagePath = require('../assets/images/bible/1Corinthians.png');
         break;
-      case "1 John":
-        bookImagePath = require("../assets/images/bible/1John.png");
+      case '1 John':
+        bookImagePath = require('../assets/images/bible/1John.png');
         break;
-      case "1 Peter":
-        bookImagePath = require("../assets/images/bible/1Peter.png");
+      case '1 Peter':
+        bookImagePath = require('../assets/images/bible/1Peter.png');
         break;
-      case "1 Thessalonians":
-        bookImagePath = require("../assets/images/bible/1Thessalonians.png");
+      case '1 Thessalonians':
+        bookImagePath = require('../assets/images/bible/1Thessalonians.png');
         break;
-      case "2 Corinthians":
-        bookImagePath = require("../assets/images/bible/1Corinthians.png");
+      case '2 Corinthians':
+        bookImagePath = require('../assets/images/bible/1Corinthians.png');
         break;
-      case "2 Thessalonians":
-        bookImagePath = require("../assets/images/bible/2Thessalonians.png");
+      case '2 Thessalonians':
+        bookImagePath = require('../assets/images/bible/2Thessalonians.png');
         break;
-      case "2 Timothy":
-        bookImagePath = require("../assets/images/bible/2Timothy.png");
+      case '2 Timothy':
+        bookImagePath = require('../assets/images/bible/2Timothy.png');
         break;
-      case "Acts":
-        bookImagePath = require("../assets/images/bible/Acts.png");
+      case 'Acts':
+        bookImagePath = require('../assets/images/bible/Acts.png');
         break;
-      case "Colossians":
-        bookImagePath = require("../assets/images/bible/Colossians.png");
+      case 'Colossians':
+        bookImagePath = require('../assets/images/bible/Colossians.png');
         break;
-      case "Ephesians":
-        bookImagePath = require("../assets/images/bible/Ephesians.png");
+      case 'Ephesians':
+        bookImagePath = require('../assets/images/bible/Ephesians.png');
         break;
-      case "Galatians":
-        bookImagePath = require("../assets/images/bible/Galatians.png");
+      case 'Galatians':
+        bookImagePath = require('../assets/images/bible/Galatians.png');
         break;
-      case "Genesis":
-        bookImagePath = require("../assets/images/bible/Genesis.png");
+      case 'Genesis':
+        bookImagePath = require('../assets/images/bible/Genesis.png');
         break;
-      case "Hebrews":
-        bookImagePath = require("../assets/images/bible/Hebrews.png");
+      case 'Hebrews':
+        bookImagePath = require('../assets/images/bible/Hebrews.png');
         break;
-      case "Isaiah":
-        bookImagePath = require("../assets/images/bible/Isaiah.png");
+      case 'Isaiah':
+        bookImagePath = require('../assets/images/bible/Isaiah.png');
         break;
-      case "James":
-        bookImagePath = require("../assets/images/bible/James.png");
+      case 'James':
+        bookImagePath = require('../assets/images/bible/James.png');
         break;
-      case "John":
-        bookImagePath = require("../assets/images/bible/John.png");
+      case 'John':
+        bookImagePath = require('../assets/images/bible/John.png');
         break;
-      case "Leviticus":
-        bookImagePath = require("../assets/images/bible/Leviticus.png");
+      case 'Leviticus':
+        bookImagePath = require('../assets/images/bible/Leviticus.png');
         break;
-      case "Luke":
-        bookImagePath = require("../assets/images/bible/Luke.png");
+      case 'Luke':
+        bookImagePath = require('../assets/images/bible/Luke.png');
         break;
-      case "Mark":
-        bookImagePath = require("../assets/images/bible/Mark.png");
+      case 'Mark':
+        bookImagePath = require('../assets/images/bible/Mark.png');
         break;
-      case "Matthew":
-        bookImagePath = require("../assets/images/bible/Matthew.png");
+      case 'Matthew':
+        bookImagePath = require('../assets/images/bible/Matthew.png');
         break;
-      case "Nahum":
-        bookImagePath = require("../assets/images/bible/Nahum.png");
+      case 'Nahum':
+        bookImagePath = require('../assets/images/bible/Nahum.png');
         break;
-      case "Numbers":
-        bookImagePath = require("../assets/images/bible/Numbers.png");
+      case 'Numbers':
+        bookImagePath = require('../assets/images/bible/Numbers.png');
         break;
-      case "Philippians":
-        bookImagePath = require("../assets/images/bible/Philippians.png");
+      case 'Philippians':
+        bookImagePath = require('../assets/images/bible/Philippians.png');
         break;
-      case "Proverbs":
-        bookImagePath = require("../assets/images/bible/Proverbs.png");
+      case 'Proverbs':
+        bookImagePath = require('../assets/images/bible/Proverbs.png');
         break;
-      case "Psalm":
-        bookImagePath = require("../assets/images/bible/Psalm.png");
+      case 'Psalm':
+        bookImagePath = require('../assets/images/bible/Psalm.png');
         break;
-      case "Romans":
-        bookImagePath = require("../assets/images/bible/Romans.png");
+      case 'Romans':
+        bookImagePath = require('../assets/images/bible/Romans.png');
         break;
 
       default:
@@ -315,8 +316,8 @@ class Verses extends React.Component {
 
     return (
       <ImageBackground
-        source={require("../assets/images/circles.png")}
-        style={{ width: "100%", height: "100%" }}
+        source={require('../assets/images/circles.png')}
+        style={{ width: '100%', height: '100%' }}
         imageStyle={{ opacity: 0.1 }}
       >
         <SafeAreaView style={styles.container}>
@@ -331,7 +332,7 @@ class Verses extends React.Component {
             <TouchableHighlight
               onPress={() => {
                 Linking.openURL(useThisVerse.verseURL).catch(err =>
-                  console.error("An error occurred", err)
+                  console.error('An error occurred', err)
                 );
               }}
               activeOpacity={75 / 100}
@@ -345,9 +346,9 @@ class Verses extends React.Component {
             </TouchableHighlight>
             <View style={styles.headerUnderline} />
           </View>
-          <View style={styles.headerBox}>
+          <ScrollView style={styles.headerBox}>
             <Text style={styles.verseText}>{useThisVerse.text}</Text>
-          </View>
+          </ScrollView>
           <View style={styles.controlsBlock}>
             <View style={[styles.headerTitle]}>
               {this.state.showVerse === 0 ? (
@@ -365,7 +366,7 @@ class Verses extends React.Component {
                   this._getVerseDates().dateTo
                 }`}
                 {useThisVerse.customDateRange ? (
-                  Platform.OS === "ios" ? (
+                  Platform.OS === 'ios' ? (
                     <View>
                       <View style={styles.dot} />
                     </View>
