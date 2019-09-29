a946622c3bf65acdf95aff1204de9560fad277be
Joshua Bechard <josh.bechard@jewsforjesus.org>
2019-09-29 01:11:38 -0500

    only grades 1 and 2 for now

--- screens/SpellingGradeSelectionScreen.js
+++ screens/SpellingGradeSelectionScreen.js
@@ -13,7 +13,7 @@ export default class SpellingGradeSelectionScreen extends React.Component {
       <BackgroundImage>
         <GradeSelector
           screenName="SpellingForGradeScreen"
-          showGrades={['first', 'second', 'third', 'fourth']}
+          showGrades={['first', 'second']}
           textColor={constants.colors.red}
         />
       </BackgroundImage>
