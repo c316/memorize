5de13e78cbc7491a7f1d9ee510852521f142bb85
Joshua Bechard <josh.bechard@jewsforjesus.org>
2019-10-24 16:25:29 -0500

    fourth grade pushed

--- screens/SpellingGradeSelectionScreen.js
+++ screens/SpellingGradeSelectionScreen.js
@@ -13,7 +13,7 @@ export default class SpellingGradeSelectionScreen extends React.Component {
       <BackgroundImage>
         <GradeSelector
           screenName="SpellingForGradeScreen"
-          showGrades={['first', 'second', 'third']}
+          showGrades={['first', 'second', 'third', 'fourth']}
           textColor={constants.colors.red}
         />
       </BackgroundImage>
