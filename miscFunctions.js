5dc4a32f04ac705ab1e0a5e44ea79e1b2a8fb06b
Joshua Bechard <josh.bechard@jewsforjesus.org>
2020-06-20 10:04:57 -0500

    John book fixes

--- miscFunctions.js
+++ miscFunctions.js
@@ -4,12 +4,19 @@ const getWeek = (date) => {
   date = date ? new Date(date) : new Date();
   date.setHours(0, 0, 0, 0);
   // Thursday in current week decides the year.
-  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
   // January 4 is always in week 1.
   let week1 = new Date(date.getFullYear(), 0, 4);
   // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-  return (1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
-    - 3 + (week1.getDay() + 6) % 7) / 7));
+  return (
+    1 +
+    Math.round(
+      ((date.getTime() - week1.getTime()) / 86400000 -
+        3 +
+        ((week1.getDay() + 6) % 7)) /
+        7
+    )
+  );
 };
 
 const { width, height } = Dimensions.get('window');
@@ -18,8 +25,9 @@ const { width, height } = Dimensions.get('window');
 const guidelineBaseWidth = 350;
 const guidelineBaseHeight = 680;
 
-const scale = size => width / guidelineBaseWidth * size;
-const verticalScale = size => height / guidelineBaseHeight * size;
-const moderateScale = (size, factor = 0.5) => size + ( scale(size) - size ) * factor;
+const scale = (size) => (width / guidelineBaseWidth) * size;
+const verticalScale = (size) => (height / guidelineBaseHeight) * size;
+const moderateScale = (size, factor = 0.5) =>
+  size + (scale(size) - size) * factor;
 
-export {getWeek, scale, verticalScale, moderateScale};
+export { getWeek, scale, verticalScale, moderateScale };
