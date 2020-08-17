614bb55a7e5df97615a6a4abe631f60ea1f91ad5
Joshua Bechard <josh.bechard@jewsforjesus.org>
2020-08-16 22:41:05 -0500

    linting

--- miscFunctions.js
+++ miscFunctions.js
@@ -1,12 +1,15 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable operator-linebreak */
 import { Dimensions } from 'react-native';
 
 const getWeek = (date) => {
+  // eslint-disable-next-line no-param-reassign
   date = date ? new Date(date) : new Date();
   date.setHours(0, 0, 0, 0);
   // Thursday in current week decides the year.
   date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
   // January 4 is always in week 1.
-  let week1 = new Date(date.getFullYear(), 0, 4);
+  const week1 = new Date(date.getFullYear(), 0, 4);
   // Adjust to Thursday in week 1 and count number of weeks from date to week1.
   return (
     1 +
