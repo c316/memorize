3fa26cd1b61d266a50f20e461b298fe8a8e1da52
Joshua Bechard <josh.bechard@jewsforjesus.org>
2020-08-24 10:17:46 -0500

    fixed week that is displayed when app is loaded

--- miscFunctions.js
+++ miscFunctions.js
@@ -11,14 +11,11 @@ const getWeek = (date) => {
   // January 4 is always in week 1.
   const week1 = new Date(date.getFullYear(), 0, 4);
   // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-  return (
-    1 +
-    Math.round(
-      ((date.getTime() - week1.getTime()) / 86400000 -
-        3 +
-        ((week1.getDay() + 6) % 7)) /
-        7
-    )
+  return Math.round(
+    ((date.getTime() - week1.getTime()) / 86400000 -
+      3 +
+      ((week1.getDay() + 6) % 7)) /
+      7
   );
 };
 
